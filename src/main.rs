8e448cfe5afef51648ab90fb9e5631d6a153ae45
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-12-01 00:27:21 -0300

    Adds ExpMod for key selection

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,72 @@
 mod player;
 
+use crate::player::Sound;
+use clap::Parser;
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use std::collections::HashMap;
 use std::error::Error;
-use std::thread;
-use std::env;
+use std::path::PathBuf;
 use std::time::Duration;
-use crate::player::SoundPlayer;
+use std::{fs, thread};
 
-
-fn key_up(_key: Keycode) {
-
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "104")]
+    total_keys: i32,
 }
 
-fn key_down(_key: Keycode, player: &SoundPlayer) -> Result<(), Box<dyn Error>> {
-    player.play()?;
+const fn key_up(_key: Keycode) {}
+
+fn key_down(key_number: i32, player: &Sound, _total_keys: i32) -> Result<(), Box<dyn Error>> {
+    player.play(key_number)?;
 
     Ok(())
 }
 
+fn get_file_paths(dir: &str) -> Vec<PathBuf> {
+    let mut file_paths = Vec::new();
+
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries.flatten() {
+            // If the entry is a file, add its path to the vector
+            if let Ok(metadata) = entry.metadata() {
+                if metadata.is_file() {
+                    file_paths.push(entry.path());
+                }
+            }
+        }
+    }
+
+    file_paths
+}
+
 fn main() {
+    let args = Args::parse();
+    let total_keys = args.total_keys;
+
     let device_state = DeviceState::new();
     let mut key_states: HashMap<Keycode, bool> = HashMap::new();
 
-    let args: Vec<String> = env::args().skip(1).collect();
-    let paths = if args.is_empty() {
-        let current_dir = env::current_dir().expect("Failed to get current directory");
-        let default_path = current_dir.join("sounds").display().to_string();
-        vec![default_path]
-    } else {
-        args
-    };
+    let paths = get_file_paths("./sounds");
 
-    let player = SoundPlayer::new(paths).unwrap();
+    let player = Sound::new(&paths, total_keys).expect("Couldn't create sound player.");
     loop {
         let keys: Vec<Keycode> = device_state.get_keys();
 
         // Check for key down events
         for key in &keys {
             if key_states.get(key).is_none() {
-                key_down(*key, &player).map_err(|err|
-                    eprintln!("ERROR: {}", err)
-                ).unwrap();
+                let key_as_i32 = *key as i32;
+                key_down(key_as_i32, &player, total_keys)
+                    .map_err(|err| eprintln!("ERROR: {err}"))
+                    .expect("Couldn't process event.");
                 key_states.insert(*key, true);
             }
         }
 
         // Check for key up events
-        let keys_up: Vec<Keycode> = key_states.keys().cloned().collect();
+        let keys_up: Vec<Keycode> = key_states.keys().copied().collect();
         for key in keys_up {
             if !keys.contains(&key) {
                 key_up(key);
--- src/player.rs
+++ src/player.rs
@@ -1,28 +1,28 @@
+use rodio::source::Buffered;
+use rodio::{Decoder, OutputStream, OutputStreamHandle, Source};
 use std::error::Error;
 use std::fs;
-use std::fs::{File, read_dir};
-use std::io::{BufReader};
-use std::path::Path;
-use rodio::{Decoder, OutputStream, OutputStreamHandle, Source};
-use rand::seq::SliceRandom;
-use rodio::source::Buffered;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
 
-pub struct SoundPlayer {
+pub struct Sound {
     _stream: OutputStream,
     handle: OutputStreamHandle,
     audio_sources: Vec<Buffered<Decoder<BufReader<fs::File>>>>,
+    total_files: i32,
+    total_keys: i32,
 }
 
-impl SoundPlayer {
-    pub fn new(paths: Vec<String>) -> Result<Self, Box<dyn Error>> {
+impl Sound {
+    pub fn new(paths: &[PathBuf], total_keys: i32) -> Result<Self, Box<dyn Error>> {
         let (stream, handle) = OutputStream::try_default()?;
 
         let mut sources = Vec::new();
 
-        for path in &paths {
-            let path = Path::new(path);
+        for path in paths {
             if path.is_dir() {
-                for entry in read_dir(path)? {
+                for entry in fs::read_dir(path)? {
                     let entry = entry?;
                     let path = entry.path();
                     if path.is_file() {
@@ -30,21 +30,35 @@ impl SoundPlayer {
                     }
                 }
             } else if path.is_file() {
-                Self::process_file(&path, &mut sources)?;
+                Self::process_file(path, &mut sources)?;
             }
         }
+        let total_files = i32::try_from(paths.len()).expect("Could not count files.");
 
         Ok(Self {
             _stream: stream,
             handle,
             audio_sources: sources,
+            total_files,
+            total_keys,
         })
     }
 
-    pub fn play(&self) -> Result<(), Box<dyn Error>> {
-        if let Some(source) = self.audio_sources.choose(&mut rand::thread_rng()) {
-            self.handle.play_raw(source.clone().convert_samples())?;
-        }
+    pub fn play(&self, key: i32) -> Result<(), Box<dyn Error>> {
+        let key = key.max(1);
+
+        let base = self.total_keys;
+        let modulus = self.total_files;
+        let exponent = key;
+
+        let index = modular_pow(base, modulus, exponent);
+
+        // Wrap the index around if it's larger than the length of audio_sources
+        #[allow(clippy::cast_sign_loss)]
+        let index = (index as usize) % self.audio_sources.len();
+
+        let source = &self.audio_sources[index];
+        self.handle.play_raw(source.clone().convert_samples())?;
 
         Ok(())
     }
@@ -58,4 +72,19 @@ impl SoundPlayer {
         }
         Ok(())
     }
+}
+
+fn modular_pow(base: i32, exponent: i32, modulus: i32) -> i32 {
+    if modulus == 1 {
+        return 0;
+    }
+
+    let mut result = 1;
+    let base_mod = base % modulus;
+
+    (0..exponent).for_each(|_| {
+        result = (result * base_mod) % modulus;
+    });
+
+    result
 }
